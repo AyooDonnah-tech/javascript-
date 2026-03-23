abd02b691cd119ade61695fc7a180b1317eea27d
AyooDonnah-tech <donnahayoo112@gmail.com>
2026-03-23 12:08:48 +0300

    my first js class

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("my name is Ayoo")
+alert("yo")
+// variables; a way of how you can store stuff in your code
+// declaring a variable uses the var, let, or const key words.
+// and assigning it a value.
+// var is function-scoped and can be redeclared and updated
+var name="Ayoo"
+console.log (name)
+name= "deee"
+console.log (name)
+// let is block-scoped and can  be updated but cannot be redeclared
+let age=23
+console.log(age)
+age= 13
+console.log(age, "gotta chop the yaers beibi")
+// const is block-scoped and cannoy be updaed or redeclared
+const country= "uganda"
+console.log(country)
+// country= "kenya"
+// onsole.log(country)
+// data types
+
+// 1.string; 
+let greeting= "hello world"
+console.log (greeting)
+console.log(typeof greeting)
+
+// 2. number
+let x= 10
+let y='5'
+console.log (x)
+console.log (typeof x)
+console.log (y)
+console.log (typeof y)
+
